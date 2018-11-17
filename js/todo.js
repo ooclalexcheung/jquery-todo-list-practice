2c3191bcc6353c5e55b9701e339c7b67632d15bc
ooclalexcheung <41135345+ooclalexcheung@users.noreply.github.com>
2018-11-18 00:22:12 +0800

    Add files via upload

--- js/todo.js
+++ js/todo.js
@@ -21,5 +21,73 @@ $(document)
             return uuid;
         }
 
+        function addToDo(){
+            var input = $( '#input' ).val();
+                $('ol').append('<li id='+ generateUUID() +'class=""><input name="done-todo" type="checkbox" class="done-todo"><span>'+ input +'</span></li>');
+                $('#input').val("");
+        }
+
+        $("#button").click(addToDo);
+
+        $("#input").keyup(function (e) {
+            if (e.keyCode == 13) {
+                addToDo();
+            }
+        });
+
+        $(document).on('change', ':checkbox', function() {
+            if ($(this).is(':checked')) {
+                $(this).parent().addClass('checked');
+            } else {
+                $(this).parent().removeClass('checked');
+            }
+        });
+
+        $(document).on("click","a[data-filter='all']",function() {
+            $("#filters li a").removeClass('selected');
+            $(this).addClass('selected');
+            $(".done-todo").each(function () {
+                $(this).parent().show();
+            })
+        })
+
+        $(document).on("click","a[data-filter='active']",function() {
+            $("#filters li a").removeClass('selected');
+            $(this).addClass('selected');
+            $("input[name='done-todo']").each(function (){
+                if($(this).is(":checked")){
+                  $(this).parent().hide();
+                }else{
+                  $(this).parent().show();
+                }
+            })
+        })
+
+        $(document).on("click","a[data-filter='complete']",function() {
+            $("#filters li a").removeClass('selected');
+                    $(this).addClass('selected');
+            $("input[name='done-todo']").each(function (){
+                if($(this).is(":checked")){
+                    $(this).parent().show();
+                }else{
+                  $(this).parent().hide();
+                }
+            })
+        })
+
+        $(document).on("dblclick","li",function(){
+            $(this)
+            .children('span').attr('contentEditable',true)
+            .keypress(function(e){
+                if(e.keyCode==13){
+                    e.target.blur();
+                    $(this)
+                    .children('span').attr('contentEditable',false);
+                }
+            })
+        })       
+
+
+
         // code to be implemented
     });
